3aae6d4a86ac892bdc7a342f4e0e8af614ffdebb
Leandra Nealer <lnealer@kingler.cs.uchicago.edu>
2021-02-17 16:15:46 -0600

    action matrix

--- scripts/q_learning.py
+++ scripts/q_learning.py
@@ -14,35 +14,36 @@ class QLearning:
     def __init__(self):
         self.counter = 0 # check if q algorithm has converged
 
-        #self.q_matrix = [0]*63 # q.matrix[i] = the index in the action matrix of optimal state 
+        self.q_matrix = [0]*64 # q.matrix[i] = the index in the action matrix of optimal state 
         
         
         # define red = 0, green = 1, blue = 2
-        # creating the action matrix
+        # define origin = 0, block 1 = 1, ....
+        self.actions = []
+        # let actions[i] = (location red, location green, location blue) in state i
+        for blue_loc in range(4):
+            for green_loc in range(4):
+                for red_loc in range(4):
+                    self.actions.append((red_loc, green_loc, blue_loc))
 
-        ## this stuff is not done
-        self.action_matrix = [[0]*63]*63
-        for begin in range(63):
-            for end in range(63):
-                if begin == end:
-                    self.action_matrix[begin][end] = -1
-                
-                new_begin = begin
-                new_end = end
-                while new_begin > 15:
-                    new_begin -= 16
-                while new_end > 15:
-                    new_end -= 16     
-                # check if the end state is divisible by 5 (these are invalid)
-                if new_end != 0 and (new_end % 5 == 0):
-                    self.action_matrix[begin][end] = -1
-                else:
-                    self.action_matrix[begin][end] = 
-                     
-                
-            
+         # creating the action matrix
+        self.action_matrix = [[-1 for i in range(64)] for j in range(64)]
+        for start in self.actions:
+            for goal in self.actions:
+                index1 = self.actions.index(start)
+                index2 = self.actions.index(goal)
 
+                diff = (start[0] != goal[0]) + (start[1] != goal[1]) + (start[2] != goal[2])
+                if diff == 1:
+                    for i in range(3):
+                        if start[i] != goal[i]:
+                            if goal[i] != 0:                 
+                                action = goal[i] - 1
+                                color = i
+                                action += color*3
+                                self.action_matrix[index1][index2] = action
 
+                    
 
     def q_algorithm(self):
         # to do
@@ -56,3 +57,4 @@ class QLearning:
 
 if __name__ == "__main__":
     Q = QLearning()
+    print(Q.action_matrix[0][0])
